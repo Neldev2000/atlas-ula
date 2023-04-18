1e143b388ffb9fb4461105ec01732f1b38362135
Nelson Vivas <nelsonandresvivaslobo@gmail.com>
2023-04-18 12:57:19 -0400

    cambiado formato de carpetas

--- pages/💲_comercial.py
+++ pages/💲_comercial.py
@@ -14,7 +14,7 @@ st.session_state['franchise_id'] = (utils.franchise_map[sucursal], sucursal)
 st.write(f" ## Ventas de {st.session_state['franchise_id'][1]}")
 tiempo = st.radio('Selecciona el tiempo a visualiazar', ['Diario', 'Semanal', 'Mensual'], horizontal=True)
 
-ventas = utils.obtener_data(file_path='.\\queries\\comercial\\ventas.sql',
+ventas = utils.obtener_data(file_path='./queries/comercial/ventas.sql',
                     columns=['fecha_venta', 'ventas'],
                     franchise_id=  st.session_state['franchise_id'][0],
                     tiempo = utils.time_translate[tiempo][0])
@@ -27,13 +27,13 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.write(f"## Ventas de este mes ya instaladas")
-    ventas_actual = utils.obtener_data(file_path='.\\queries\\comercial\\clientes_instalados_este_mes.sql',
+    ventas_actual = utils.obtener_data(file_path='./queries/comercial/clientes_instalados_este_mes.sql',
                     columns=['ventas'],
                     franchise_id=  st.session_state['franchise_id'][0]) 
     st.write(f"## {ventas_actual.loc[0, ['ventas']].values[0]}")
 with col2:
     st.write(f"## Ventas del mes pasado ya instaladas")
-    ventas_pasadas = utils.obtener_data(file_path='.\\queries\\comercial\\clientes_del_mes_pasado.sql',
+    ventas_pasadas = utils.obtener_data(file_path='./queries/comercial/clientes_del_mes_pasado.sql',
                     columns=['ventas'],
                     franchise_id=  st.session_state['franchise_id'][0]) 
     st.write(f"## {ventas_pasadas.loc[0, ['ventas']].values[0]}")
