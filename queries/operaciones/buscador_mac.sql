980642a78904ea18e73a7544ec1da96f757ca793
Nelson Vivas <nelsonandresvivaslobo@gmail.com>
2023-04-18 21:14:04 -0400

    mejorar buscador

--- queries/operaciones/buscador_mac.sql
+++ queries/operaciones/buscador_mac.sql
@@ -11,6 +11,6 @@ cross join json_array_elements(wan_config) as elm
 join contract_contacts as cc using(contract_id)
 join contract_addresses as ca using(contract_id)
 where 
-	lower((select MAC from variables)) like concat('%',lower(elm->>'MAC_ONU\/ONT'))
-	and concat('%',lower(elm->>'MAC_ONU\/ONT')) != '%'
+	lower((select MAC from variables)) like concat('%',lower(elm->>'MAC_ONU/ONT'))
+	and concat('%',lower(elm->>'MAC_ONU/ONT')) != '%'
 	and ca.type = 'tech'
